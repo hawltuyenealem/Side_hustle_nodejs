6ec5af4b186866d1dad1e6fb5ab1d0c68d5ef66c
Hawltu <yenealemhawltu@gmail.com>
2022-04-27 11:20:56 -0700

    some final fixes

--- Group_56_capstone1/server.js
+++ Group_56_capstone1/server.js
@@ -1,42 +1,51 @@
 const http = require('http');
-
+const fs = require('fs')
 const host  = '127.0.0.1';
 const port  = 5000;
 const os  = require('os');
+const osinfo  = JSON.stringify(
+    {
+        "host_name" : os.hostname,
+        "platform" : os.platform,
+        "artchitecture" : os.artchitecture,
+        "numberofCPUS": os.numberofCPUS,
+        "netwokrkInterfaces": os.networkInterfaces,
+        "uptime": os.uptime,
+
+    }
+);
 const server  = http.createServer((req,res) =>{
     const urlPath  = req.url;
 
-
+    
     if (urlPath == '/'){
         res.statusCode = 200;
         res.setHeader('Content-Type','text/html');
+        
+        res.writeHead(200, { location : "/pages/index.html" });
+        return res.end();
      
     }else if (urlPath == '/about'){
         res.statusCode = 200;
-        res.setHeader('Content-Type','text/plain');
+        res.setHeader('Content-Type','text/html');
+        res.writeHead(200, { location : "/pages/about.html"});
+        return res.end();
 
     }else if(urlPath == '/sys'){
-        res.statusCode = 200;
-        res.setHeader('Content-Type','text/plain');
+        res.statusCode = 201;
+        res.setHeader('Content-Type','application/json');
+        res.write(osinfo);
+        return res.end("Your OS info has been saved successfully!");
 
     }else {
         res.statusCode = 404;
         res.setHeader('Content-Type','text/html');
-        res.end('Page not Found !!');
+        res.writeHead(404, {location: '/pages/404.html'});
+        return res.end('Page not Found !!');
     }
 });
 
-const data = JSON.stringify(
-    {
-        host_name : os.hostname,
-        platform : os.platform,
-        artchitecture : os.artchitecture,
-        numberofCPUS: os.numberofCPUS,
-        netwokrkInterfaces: os.networkInterfaces,
-        uptime: os.uptime,
 
-    }
-);
 
 server.listen(port,host,() => {
     console.log(`Running at  ${host}:${port}`);
